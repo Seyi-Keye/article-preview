5906f5c2dc190a24c1ac5a9b3189ce66e22c5acc
Oluwaseyi Aromokeye <aromokeyes2@gmail.com>
2020-07-10 10:53:24 +0100

    create round avatar v2

--- src/Image.js
+++ src/Image.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Image(props) {
   return (
-    <div className="b-image">
-      <img src={props.src} className={props.className} alt={props.alt} />
-    </div>
+    // <div className="b-image">
+    <img src={props.src} className={props.className} alt={props.alt} />
+    // </div>
   );
 }
 
